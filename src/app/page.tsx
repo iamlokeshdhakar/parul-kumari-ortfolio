f16b607fb7154b7efd15a08b73e779627b0e3dce
Lokesh Dhakar <lokesh.dhakar@axy-app.com>
2026-08-31 11:20:53 +0530

    add projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <About />
         <Skills />
         <Experience />
-        {/* <Projects /> */}
+        <Projects />
       </main>
       <Footer />
     </>
